3981301dd09c9da4e407a36705dc6c1c4013a9fc
Emily <emily.nunez@correounivalle.edu.co>
2025-11-07 04:13:46 -0500

    BD update

--- app/src/main/java/com/moviles/proyecto1/repository/InventoryRepository.kt
+++ app/src/main/java/com/moviles/proyecto1/repository/InventoryRepository.kt
@@ -3,7 +3,33 @@ package com.moviles.proyecto1.repository
 import android.content.Context
 import com.moviles.proyecto1.data.InventoryDB
 import com.moviles.proyecto1.data.InventoryDao
+import com.moviles.proyecto1.model.Inventory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class InventoryRepository (val context: Context) {
     private var inventoryDao: InventoryDao = InventoryDB.getDatabase(context).inventoryDao()
+    suspend fun saveInventory(inventory:Inventory){
+        withContext(Dispatchers.IO){
+            inventoryDao.saveInventory(inventory)
+        }
+    }
+
+    suspend fun getListInventory():MutableList<Inventory>{
+        return withContext(Dispatchers.IO){
+            inventoryDao.getListInventory()
+        }
+    }
+
+    suspend fun deleteInventory(inventory: Inventory){
+        withContext(Dispatchers.IO){
+            inventoryDao.deleteInventory(inventory)
+        }
+    }
+
+    suspend fun updateRepository(inventory: Inventory){
+        withContext(Dispatchers.IO){
+            inventoryDao.updateInventory(inventory)
+        }
+    }
 }
---
+++ app/src/main/java/com/moviles/proyecto1/view/Widget.kt
@@ -0,0 +1,4 @@
+package com.moviles.proyecto1.view
+
+class Widget {
+}
