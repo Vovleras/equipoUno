132d6e86c368b5271a8f2763a0e5e56e139a09cd
SheilaV7 <sheila.valencia@correounivalle.edu.co>
2025-11-07 10:33:11 -0500

    Cargar los productos desde la BD, funcionalidad de barra de progreso y cambio de nombre funciones

--- app/src/main/java/com/moviles/proyecto1/view/fragment/HomeInventory.kt
+++ app/src/main/java/com/moviles/proyecto1/view/fragment/HomeInventory.kt
@@ -12,17 +12,27 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.moviles.proyecto1.R
 import com.moviles.proyecto1.databinding.FragmentHomeInventoryBinding
 import com.moviles.proyecto1.model.Inventory
+import com.moviles.proyecto1.viewmodel.InventoryViewModel
 import com.moviles.proyecto1.view.adapter.RecyclerAdapter
 import android.content.Context
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
 
 class HomeInventory : Fragment() {
     private lateinit var binding: FragmentHomeInventoryBinding
+    private val inventoryViewModel: InventoryViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeInventoryBinding.inflate(inflater)
-        //navigationHomeInventoryToDetails()
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         if (verifySession()) {
             navigationHomeInventoryToAdd()
             listInventory()
@@ -32,33 +42,35 @@ class HomeInventory : Fragment() {
             findNavController().navigate(R.id.action_homeInventory_to_login)
         }
 
-        binding.lifecycleOwner = this
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
             requireActivity().moveTaskToBack(true)
         }
     }
 
     private fun listInventory() {
-        var listProducts = mutableListOf(
-            Inventory(4,"zapatos", 100.0f, 5),
-            Inventory(5,"camisas", 100.0f, 3),
-        )
+        //Agregar productos mientras, eliminar cuando este add
 
-        val recycler = binding.rvProducts
-        recycler.layoutManager = LinearLayoutManager(context)
-        val adapter = RecyclerAdapter(listProducts,  findNavController())
-        recycler.adapter = adapter
-        adapter.notifyDataSetChanged()
+        val dato1 = Inventory(4,"zapatos", 100.0f, 5)
+        val dato2 = Inventory(5,"camisas", 100.0f, 3)
+        inventoryViewModel.saveInventory(dato1)
+        inventoryViewModel.saveInventory(dato2)
+
+        // Borrar hasta aqui
+
+        inventoryViewModel.getListInventory()
+        inventoryViewModel.listInventory.observe(viewLifecycleOwner) { listInventory ->
+            val recycler = binding.rvProducts
+            recycler.layoutManager = LinearLayoutManager(context)
+            val adapter = RecyclerAdapter(listInventory,  findNavController())
+            recycler.adapter = adapter
+            adapter.notifyDataSetChanged()
+        }
     }
 
     private fun navigationHomeInventoryToAdd(){
         binding.btnAdd.setOnClickListener {
+            val dato = Inventory(20,"pantalones", 100.0f, 3) //Borrar
+            inventoryViewModel.saveInventory(dato)
             findNavController().navigate(R.id.action_homeInventory_to_addProduct)
         }
     }
@@ -85,6 +97,12 @@ class HomeInventory : Fragment() {
     private fun verifySession(): Boolean {
         val session = requireActivity().getSharedPreferences("user_session", Context.MODE_PRIVATE)
         return session.getBoolean("is_logged_in", false)
+    }
+
+    private fun progressDB() {
+        inventoryViewModel.progresState.observe(viewLifecycleOwner){status ->
+            binding.progress.isVisible = status
+        }
     }
 
 }
