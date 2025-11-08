c227f7a9215b0fdb20e22d81c0488505a57e5df7
Salome Acosta Montaño <salome.acosta@correounivalle.edu.co>
2025-11-07 23:29:28 -0500

    Fields and button verification

--- app/src/main/java/com/moviles/proyecto1/view/fragment/AddProduct.kt
+++ app/src/main/java/com/moviles/proyecto1/view/fragment/AddProduct.kt
@@ -1,12 +1,18 @@
 package com.moviles.proyecto1.view.fragment
 
+import android.graphics.Typeface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isEmpty
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
+import androidx.navigation.fragment.findNavController
 import com.moviles.proyecto1.R
 import com.moviles.proyecto1.databinding.FragmentAddProductBinding
+import com.moviles.proyecto1.databinding.FragmentDetailProductBinding
 
 class AddProduct : Fragment() {
     private lateinit var binding: FragmentAddProductBinding
@@ -15,7 +21,52 @@ class AddProduct : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAddProductBinding.inflate(inflater)
+        setupToolbar()
+        binding.btnGuardar.isEnabled = false
         binding.lifecycleOwner = this
+
+        setupFieldListeners()
+
+
         return binding.root
+
     }
+
+    private fun setupToolbar(){
+        binding.contentToolbar.title = "Agregar Producto"
+        binding.contentToolbar.toolbar.setNavigationOnClickListener {
+            findNavController().navigate(R.id.action_addProduct_to_homeInventory)
+        }
+
+    }
+
+    private fun validateFields() {
+        val codigo = binding.editCodigoProducto.text?.toString()?.trim()
+        val nombre = binding.editNombreArticulo.text?.toString()?.trim()
+        val precio = binding.editPrecio.text?.toString()?.trim()
+        val cantidad = binding.editCantidad.text?.toString()?.trim()
+
+        val btnGuardar = binding.btnGuardar
+
+        val areFilled = !codigo.isNullOrEmpty() &&
+                !nombre.isNullOrEmpty() &&
+                !precio.isNullOrEmpty() &&
+                !cantidad.isNullOrEmpty()
+
+        btnGuardar.apply {
+            isEnabled = areFilled
+            setTypeface(null, if (areFilled) Typeface.BOLD else Typeface.NORMAL)
+        }
+
+    }
+
+    private fun setupFieldListeners() {
+        binding.EDcodigoProducto.editText?.addTextChangedListener { validateFields() }
+        binding.EDnombreArticulo.editText?.addTextChangedListener { validateFields() }
+        binding.EDprecio.editText?.addTextChangedListener { validateFields() }
+        binding.EDcantidad.editText?.addTextChangedListener { validateFields() }
+    }
+
+
+
 }
