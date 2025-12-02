4c0739f4436d67984eae945844706c0aa893a328
Emily <emily.nunez@correounivalle.edu.co>
2025-12-01 23:25:36 -0500

    Adelanto login/registro

--- app/src/main/java/com/moviles/proyecto1/view/LoginActivity.kt
+++ app/src/main/java/com/moviles/proyecto1/view/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.moviles.proyecto1.view
 
 import android.graphics.Typeface
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
@@ -10,18 +11,81 @@ import com.moviles.proyecto1.R
 import com.moviles.proyecto1.databinding.ActivityLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.core.widget.doOnTextChanged
-
+import com.moviles.proyecto1.viewmodel.LoginViewModel
+import androidx.activity.viewModels
+import com.moviles.proyecto1.model.UserRequest
+import android.content.Context
 
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
 
+    companion object {
+        private const val PREFS_NAME = "user_session"
+        private const val KEY_IS_LOGGED_IN = "is_logged_in"
+        private const val COME_WIDGET = "from_widget"
+        private const val KEY_IS_FROM_WIDGET = "is_from_widget"
+    }
+
+    private val loginViewModel: LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         setupListeners()
 
+    }
+
+    private fun saveUserSession() {
+        val sharedPref = this.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        with(sharedPref.edit()) {
+            putBoolean(KEY_IS_LOGGED_IN, true)
+            apply()
+        }
+    }
+
+    private fun isUserLoggedIn(): Boolean {
+        val sharedPref = this.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        return sharedPref.getBoolean(KEY_IS_LOGGED_IN, false)
+    }
+
+    private fun isFromWidget(): Boolean {
+        val sharedPref = this.getSharedPreferences(COME_WIDGET, Context.MODE_PRIVATE)
+        return sharedPref.getBoolean(KEY_IS_FROM_WIDGET, false)
+    }
+
+    private fun registerUser(){
+        val email = binding.IEmail.text.toString().trim()
+        val pass = binding.IPass.text.toString().trim()
+        val userRequest = UserRequest(email, pass)
+
+        if(email.isNotEmpty() && pass.isNotEmpty()){
+            loginViewModel.registerUser(userRequest)
+            loginViewModel.isRegister.observe(this){ userResponse ->
+                if (userResponse.isRegister){
+                    saveUserSession()
+                    Toast.makeText(this, userResponse.message, Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this, userResponse.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun loginUser(){
+        val email = binding.IEmail.text.toString().trim()
+        val pass = binding.IPass.text.toString().trim()
+
+        loginViewModel.loginUser(email, pass) { isLogin ->
+            if (isLogin) {
+                saveUserSession()
+                Toast.makeText(this, "Login Exitoso", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login incorrecto", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun setupListeners() {
@@ -79,4 +143,5 @@ class LoginActivity : AppCompatActivity() {
             binding.TFPass.error = null
         }
     }
+
 }
